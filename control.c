0b5a952d125597413808de1d7059cb6ed024804a
makpolatt <emreakpolat3@gmail.com>
2025-03-04 14:24:03 +0300

    coin, karakter, ve kapı kontrolleri eklendi

--- control.c
+++ control.c
@@ -6,13 +6,13 @@
 /*   By: makpolat <makpolat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 08:28:01 by makpolat          #+#    #+#             */
-/*   Updated: 2025/03/04 13:25:09 by makpolat         ###   ########.fr       */
+/*   Updated: 2025/03/04 14:23:41 by makpolat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void coin_check(char **map)
+static void coin_check(char **map)
 {
     int i;
     int j = 0;
@@ -32,16 +32,61 @@ void coin_check(char **map)
         i++;
     }
     if (coin_count < 1)
-        error("There is no coin!\n");
+        error("Error. There is no coin!\n");
 }
 
+static void     character_check(char **map)
+{
+    int i;
+    int j;
+    int character_count;
 
+    i = 0;
+    character_count = 0;
+    while (map[i])
+    {
+        j = 0;
+        while (map[i][j])
+        {
+            if (map[i][j] == 'P')
+                character_count++;
+            j++;
+        }
+        i++;
+    }
+    if (character_count != 1)
+        error("Error. There is no character\n");
+}
+
+static void     door_check(char **map)
+{
+    int i;
+    int j;
+    int door_count;
+
+    i = 0;
+    door_count = 0;
+    while (map[i])
+    {
+        j = 0;
+        while (map[i][j])
+        {
+            if (map[i][j] == 'E')
+                door_count++;
+            j++;
+        }
+        i++;
+    }
+    if (door_count != 1)
+        error("Error. There is no character\n");
+}
 
 void    map_control(char **map)
 {
     coin_check(map);
+    character_check(map);
+    door_check()
     //wall_check()
-    //character_check()
     
 
 }
