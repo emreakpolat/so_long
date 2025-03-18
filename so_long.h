890bfb3075a566cbf9a7f5499a3fe524416ffd6c
makpolatt <emreakpolat3@gmail.com>
2025-03-18 12:38:43 +0300

    so_long bitişe yaklaşıldı

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: makpolat <makpolat@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 08:14:35 by makpolat          #+#    #+#             */
-/*   Updated: 2025/03/18 12:33:24 by makpolat         ###   ########.fr       */
+/*   Updated: 2025/03/18 12:35:04 by makpolat         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,14 +48,11 @@ char		**gnl_read(int fd, char **av, int i);
 void		close_check(char **map, int y, int i, int j);
 void		error(char *str);
 void		free_map(char **map);
-
 void		map_control(char **map);
 void		flood_fill_check(char **map, int i, int x, int y);
 void		mlx_all_add(char **map, t_general *game);
 void		rectangle_check(char **map, int i, int x, int y);
-
 void		find_collectible_and_player(t_general *game, char **map);
-
 t_general	create_map(t_general game, char **map);
 
 #endif
